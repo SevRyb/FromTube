9472089b9500fdf2fb1302e7fe6df669235071ca
SevRyb <rybka.severus@gmail.com>
2022-11-28 23:51:41 +0200

    Started working on main window

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -6,10 +6,12 @@
 #include <QPushButton>
 #include <QLineEdit>
 #include <QTextEdit>
+#include <QComboBox>
 #include <QLabel>
 #include <QVideoWidget>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QGridLayout>
 #include <QDebug>
 
 class MainWindow : public QWidget
@@ -21,13 +23,48 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 private:
-    QFrame *m_videoFrame;
-    QVideoWidget *m_videoWidget;
+    QVBoxLayout *m_mainVBoxLay;
 
+    /* Media Area */
+    QVideoWidget *m_videoWidget;
+    QHBoxLayout *m_videoHBoxLay;
+    QFrame *m_videoFrame;
+
+    /* Page Url Area */
+    QLineEdit *m_pageUrlLEdit;
+    QPushButton *m_findBtn;
+    QHBoxLayout *m_pageUrlHBoxLay;
     QFrame *m_pageUrlFrame;
 
+    /* Direct Url Area */
+    QLineEdit *m_directUrlLEdit;
+    QPushButton *m_copyBtn;
+    QHBoxLayout *m_directUrlHBoxLay;
     QFrame *m_directUrlFrame;
+
+    /* Format Area */
+    QComboBox *m_audioFormatComboBox;
+    QComboBox *m_videoFormatComboBox;
+    QComboBox *m_outFormatComboBox;
+    //QHBoxLayout *m_mediaFormatHBoxLay;
+    QGridLayout *m_mediaFormatGridLay;
     QFrame *m_mediaFormatFrame;
+
+    /* File Name */
+    QLineEdit *m_fileNameLEdit;
+
+    /* Download Dir Area */
+    QLineEdit *m_downloadDirLEdit;
+    QPushButton *m_openBtn;
+    QHBoxLayout *m_downloadDirHBoxLay;
+    QFrame *m_downloadDirFrame;
+
+    /* Download Button */
+    QPushButton *m_downloadBtn;
+
+    /* Log Text */
+    QTextEdit *m_logTextEdit;
+
 
 };
 #endif // MAINWINDOW_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -8,18 +8,97 @@ MainWindow::MainWindow(QWidget *parent)
 
     resize(600, 600);
 
-    QTextEdit* text_edit = new QTextEdit(this);
+    m_mainVBoxLay = new QVBoxLayout();
+    //m_mainVBoxLay->setMargin(0);
+    setLayout(m_mainVBoxLay);
 
-    QPushButton* btn = new QPushButton(this);
-    btn->setText(tr("Hello world! %1").arg(100));
+    /* Media Area */
+    m_videoWidget = new QVideoWidget(this);
+    m_videoHBoxLay = new QHBoxLayout();
+    m_videoHBoxLay->setMargin(0);
+    m_videoHBoxLay->addWidget(m_videoWidget);
+    m_videoFrame = new QFrame(this);
+    m_videoFrame->setLayout(m_videoHBoxLay);
 
-    QVBoxLayout* main_lay = new QVBoxLayout();
-    main_lay->addWidget(text_edit);
-    main_lay->addWidget(btn);
+    /* Page Url Area */
+    m_pageUrlLEdit = new QLineEdit(this);
+    m_findBtn = new QPushButton("Find", this);
+    m_pageUrlHBoxLay = new QHBoxLayout();
+    m_pageUrlHBoxLay->setMargin(0);
+    m_pageUrlHBoxLay->addWidget(m_pageUrlLEdit);
+    m_pageUrlHBoxLay->addWidget(m_findBtn);
+    m_pageUrlFrame = new QFrame(this);
+    m_pageUrlFrame->setLayout(m_pageUrlHBoxLay);
 
-    setLayout(main_lay);
+    /* Direct Url Area */
+    m_directUrlLEdit = new QLineEdit(this);
+    m_copyBtn = new QPushButton("Copy", this);
+    m_directUrlHBoxLay = new QHBoxLayout();
+    m_directUrlHBoxLay->setMargin(0);
+    m_directUrlHBoxLay->addWidget(m_directUrlLEdit);
+    m_directUrlHBoxLay->addWidget(m_copyBtn);
+    m_directUrlFrame = new QFrame(this);
+    m_directUrlFrame->setLayout(m_directUrlHBoxLay);
 
-    connect(btn, &QPushButton::pressed, this, &MainWindow::findUrl);
+    /* Format Area */
+    m_audioFormatComboBox = new QComboBox(this);
+    m_videoFormatComboBox = new QComboBox(this);
+    m_outFormatComboBox = new QComboBox(this);
+    m_mediaFormatGridLay = new QGridLayout();
+    m_mediaFormatGridLay->setMargin(0);
+    m_mediaFormatGridLay->setColumnStretch(0, 1);
+    m_mediaFormatGridLay->setColumnStretch(2, 1);
+    m_mediaFormatGridLay->setColumnStretch(4, 1);
+    m_mediaFormatGridLay->addWidget(new QLabel("Audio", this), 0, 0);
+    m_mediaFormatGridLay->addWidget(new QLabel("Video", this), 0, 2);
+    m_mediaFormatGridLay->addWidget(new QLabel("Out", this), 0, 4);
+    m_mediaFormatGridLay->addWidget(m_audioFormatComboBox, 1, 0);
+    m_mediaFormatGridLay->addWidget(new QLabel("+", this), 1, 1, Qt::AlignCenter);
+    m_mediaFormatGridLay->addWidget(m_videoFormatComboBox, 1, 2);
+    m_mediaFormatGridLay->addWidget(new QLabel("=", this), 1, 3, Qt::AlignCenter);
+    m_mediaFormatGridLay->addWidget(m_outFormatComboBox, 1, 4);
+    m_mediaFormatFrame = new QFrame(this);
+    m_mediaFormatFrame->setLayout(m_mediaFormatGridLay);
+
+    /* File Name */
+    m_fileNameLEdit = new QLineEdit(this);
+
+    /* Download Dir Area */
+    m_downloadDirLEdit = new QLineEdit(this);
+    m_openBtn = new QPushButton("Open", this);
+    m_downloadDirHBoxLay = new QHBoxLayout();
+    m_downloadDirHBoxLay->setMargin(0);
+    m_downloadDirHBoxLay->addWidget(m_downloadDirLEdit);
+    m_downloadDirHBoxLay->addWidget(m_openBtn);
+    m_downloadDirFrame = new QFrame(this);
+    m_downloadDirFrame->setLayout(m_downloadDirHBoxLay);
+
+    /* Log Text */
+    m_logTextEdit = new QTextEdit(this);
+
+    /* Download Button */
+    m_downloadBtn = new QPushButton("Download", this);
+    m_downloadBtn->setFixedSize(80, 40);
+
+
+    /* Layouts */
+    m_mainVBoxLay->addWidget(m_videoFrame);
+    m_mainVBoxLay->addWidget(new QLabel("Page url", this));
+    m_mainVBoxLay->addWidget(m_pageUrlFrame);
+    m_mainVBoxLay->addWidget(new QLabel("Direct url", this));
+    m_mainVBoxLay->addWidget(m_directUrlFrame);
+    //m_mainVBoxLay->addWidget(new QLabel("Format", this));
+    m_mainVBoxLay->addWidget(m_mediaFormatFrame);
+    m_mainVBoxLay->addWidget(new QLabel("Name", this));
+    m_mainVBoxLay->addWidget(m_fileNameLEdit);
+    m_mainVBoxLay->addWidget(new QLabel("Download directory", this));
+    m_mainVBoxLay->addWidget(m_downloadDirFrame);
+    m_mainVBoxLay->addWidget(m_downloadBtn, 0, Qt::AlignRight);
+    m_mainVBoxLay->addWidget(new QLabel("Log", this));
+    m_mainVBoxLay->addWidget(m_logTextEdit);
+
+
+    //connect(btn, &QPushButton::pressed, this, &MainWindow::findUrl);
 }
 
 
